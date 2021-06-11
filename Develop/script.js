d14a21ba5f443904bb63413aae932221011758a3
Chase Rivera <achaserivera@gmail.com>
2021-06-10 17:47:43 -0700

    couldn't figure out how have my grid show up having it in JS, so i decided to make a div in the html that will have unique ids for every hour of the work day. also adding text area and a button class to these divs.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,3 @@
-//var timeBlocksContainerEL = $("#timeBlocksContainer");
-
-//var currentDayEl = $("#currentDay");
-
-//var workHours = 9;
-
-//var startTime = 9;
 
 var liveDate = moment().format("MMMM/Do/YYYY");
 $("#currentDay").text(liveDate)
